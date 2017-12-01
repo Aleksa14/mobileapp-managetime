9d6e94de0814f4ac7602dee689c02d81919d9904
Aleksa14 <ola.orzechowska7@gmail.com>
2017-12-01 23:23:08 +0100

    applay button in settings

--- app/src/main/java/com/example/olaor/taskmanager/Settings.java
+++ app/src/main/java/com/example/olaor/taskmanager/Settings.java
@@ -1,6 +1,8 @@
 package com.example.olaor.taskmanager;
 
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,13 +12,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import com.example.olaor.taskmanager.TaskManager.Data.Project;
+import com.example.olaor.taskmanager.TaskManager.Schedulers.TimeLine;
+
 import java.util.Calendar;
+import java.util.List;
 
 public class Settings extends Fragment implements View.OnClickListener {
 
+    public static final String SETTINGS_PREF = "SETTINGS_PREF";
+    public static final String SETTINGS_START = "SETTINGS_START";
+    public static final String SETTINGS_END = "SETTINGS_END";
     private EditText startTime, endTime;
     private Button applyButton;
     private int mHour, mMinute;
+    SharedPreferences sharedpreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
@@ -52,7 +62,23 @@ public class Settings extends Fragment implements View.OnClickListener {
             timePickerDialog.show();
         }
         if (view == applyButton){
-
+            long start = Long.parseLong(startTime.getText().toString()) * 60 * 60 * 1000;
+            long end = Long.parseLong(endTime.getText().toString()) * 60 * 60 * 1000;
+            Context context = getActivity();
+            sharedpreferences = context.getSharedPreferences(SETTINGS_PREF, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putLong(SETTINGS_START, start);
+            editor.putLong(SETTINGS_END, end);
+            editor.commit();
+            new Thread(){
+                @Override
+                public void run(){
+                    List<Project> projectList = TimeLine.db.projectDao().getAllProject();
+                    for (Project p : projectList){
+                        TimeLine.sheduleNewProject(p, getContext(), System.currentTimeMillis());
+                    }
+                }
+            }.start();
         }
     }
 
